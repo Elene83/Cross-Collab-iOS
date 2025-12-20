9b24c424994728b1efa1e497857f5b110bee92e9
Elene <elenedgebuadze83@gmail.com>
2025-12-20 10:39:35 +0400

    Add unnecessary files to gitignore

---
+++ EventManager/EventManager/EventManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventManagerApp.swift
+//  EventManager
+//
+//  Created by Eorime on 20.12.25.
+//
+
+import SwiftUI
+
+@main
+struct EventManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
